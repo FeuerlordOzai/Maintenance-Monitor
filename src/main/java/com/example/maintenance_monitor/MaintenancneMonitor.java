153c75f1aa28be5e32243fe610927ce32694eb64
Malek <malekbouazizi@gmail.com>
2022-06-17 01:32:51 +0200

    add Tests

--- src/main/java/com/example/maintenance_monitor/MaintenancneMonitor.java
+++ src/main/java/com/example/maintenance_monitor/MaintenancneMonitor.java
@@ -3,7 +3,7 @@ package com.example.maintenance_monitor;
 public class MaintenancneMonitor {
 
     private static final String DEFAULT_MESSAGE = "Message wurde geresettet";
-    private String message;
+    private String message = "";
     public String getMessage()
     {
         return message;
